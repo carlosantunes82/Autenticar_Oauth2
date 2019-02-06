ac6cb72b62b7e7534b4b05be245db1cb148cbaa8
Carlos Antunes <CEAntunes.WA@rd.com.br>
2019-02-06 19:44:03 -0200

    Construcao proxy castro cliente PBM

---
+++ src/main/java/br/com/raiadrogasil/cadastroclientepbmrproxy/dto/CadastroClientePbmrDto.java
@@ -0,0 +1,318 @@
+package br.com.raiadrogasil.cadastroclientepbmrproxy.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CadastroClientePbmrDto implements Serializable {
+
+    private String acao;
+
+    private String idCliente;
+
+    private String nome;
+
+    private String cpf;
+
+    private String dataNascimento;
+
+    private String sexo;
+
+    private String tipoLogradouro; // VERIFICAR
+
+    private String endereco;
+
+    private String numero;
+
+    private String complemento;
+
+    private String cep;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String uf;
+
+    private String dddTelefone;
+
+    private String telefone;
+
+    private String dddCelular;
+
+    private String celular;
+
+    private String email;
+
+    private String medicoCrm;
+
+    private String medicoNome;
+
+    private String medicoUf;
+
+    private String contatoEmail;
+
+    private String contatoCelular;
+
+    private String contatoCorreio;
+
+    private String contatoPermissao;
+
+    private String cdProduto;
+
+    private String precoBruto;
+
+    private String precoLiquido;
+
+    private String isContatos;
+
+    private String nrSequenciaEndereco;
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public void setAcao(String acao) {
+        this.acao = acao;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getDddTelefone() {
+        return dddTelefone;
+    }
+
+    public void setDddTelefone(String dddTelefone) {
+        this.dddTelefone = dddTelefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDddCelular() {
+        return dddCelular;
+    }
+
+    public void setDddCelular(String dddCelular) {
+        this.dddCelular = dddCelular;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMedicoCrm() {
+        return medicoCrm;
+    }
+
+    public void setMedicoCrm(String medicoCrm) {
+        this.medicoCrm = medicoCrm;
+    }
+
+    public String getMedicoNome() {
+        return medicoNome;
+    }
+
+    public void setMedicoNome(String medicoNome) {
+        this.medicoNome = medicoNome;
+    }
+
+    public String getMedicoUf() {
+        return medicoUf;
+    }
+
+    public void setMedicoUf(String medicoUf) {
+        this.medicoUf = medicoUf;
+    }
+
+    public String getContatoEmail() {
+        return contatoEmail;
+    }
+
+    public void setContatoEmail(String contatoEmail) {
+        this.contatoEmail = contatoEmail;
+    }
+
+    public String getContatoCelular() {
+        return contatoCelular;
+    }
+
+    public void setContatoCelular(String contatoCelular) {
+        this.contatoCelular = contatoCelular;
+    }
+
+    public String getContatoCorreio() {
+        return contatoCorreio;
+    }
+
+    public void setContatoCorreio(String contatoCorreio) {
+        this.contatoCorreio = contatoCorreio;
+    }
+
+    public String getContatoPermissao() {
+        return contatoPermissao;
+    }
+
+    public void setContatoPermissao(String contatoPermissao) {
+        this.contatoPermissao = contatoPermissao;
+    }
+
+    public String getCdProduto() {
+        return cdProduto;
+    }
+
+    public void setCdProduto(String cdProduto) {
+        this.cdProduto = cdProduto;
+    }
+
+    public String getPrecoBruto() {
+        return precoBruto;
+    }
+
+    public void setPrecoBruto(String precoBruto) {
+        this.precoBruto = precoBruto;
+    }
+
+    public String getPrecoLiquido() {
+        return precoLiquido;
+    }
+
+    public void setPrecoLiquido(String precoLiquido) {
+        this.precoLiquido = precoLiquido;
+    }
+
+    public String getIsContatos() {
+        return isContatos;
+    }
+
+    public void setIsContatos(String isContatos) {
+        this.isContatos = isContatos;
+    }
+
+    public String getNrSequenciaEndereco() {
+        return nrSequenciaEndereco;
+    }
+
+    public void setNrSequenciaEndereco(String nrSequenciaEndereco) {
+        this.nrSequenciaEndereco = nrSequenciaEndereco;
+    }
+
+}
