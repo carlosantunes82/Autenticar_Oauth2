50a9cb4435c14e9945e4a16c199542ab7919564e
Carlos Antunes <CEAntunes.WA@rd.com.br>
2019-02-11 14:37:22 -0200

    Criacao proxy PMB

--- src/main/java/br/com/raiadrogasil/cadastroclientepbmrproxy/exceptionhandlers/CustomExceptionHandler.java
+++ src/main/java/br/com/raiadrogasil/cadastroclientepbmrproxy/exceptionhandlers/CustomExceptionHandler.java
@@ -2,21 +2,31 @@ package br.com.raiadrogasil.cadastroclientepbmrproxy.exceptionhandlers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomExceptionHandler { //extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     protected ResponseEntity handleConstraintViolation(ConstraintViolationException ex, WebRequest request){
@@ -30,4 +40,31 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                                 violation -> violation.getMessage())));
     }
 
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity validationError(BindException ex) {
+
+        return ResponseEntity
+                .badRequest()
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .body(ex.getBindingResult()
+                        .getAllErrors()
+                        .stream()
+                        .collect(Collectors.toMap(
+                                msg -> msg.getCode(),
+                                msg -> msg.getDefaultMessage()
+                        )));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity validationError(Exception ex) {
+
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
 }
+
+
+
